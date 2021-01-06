887015d626a5e0bcab0a7b69da0286b3e46d1eae
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-06 03:35:51 -0300

    feat: ensure SignupController calls AddAccount with correct values

---
+++ src/domain/data-transfer-objects/account-dto.ts
@@ -0,0 +1,5 @@
+export interface AccountDTO {
+  name: string,
+  email: string,
+  password: string
+}
--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,15 +1,17 @@
+import { AddAccount } from '../../../domain/usecases/add-account'
 import { Controller, Request, Response, EmailValidator } from '../../contracts'
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest, serverError } from '../../helpers/http-helper'
 
 export class SignupController implements Controller {
   constructor (
-    private readonly emailValidator: EmailValidator
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccount: AddAccount
   ) {}
 
   handle (request: Request): Response {
     try {
-      const { email, password, confirmation } = request.body
+      const { name, email, password, confirmation } = request.body
       const requiredFields = ['name', 'email', 'password', 'confirmation']
       for (const field of requiredFields) {
         if (!request.body[field]) {
@@ -23,6 +25,7 @@ export class SignupController implements Controller {
       if (!isEmail) {
         return badRequest(new InvalidParamError('email'))
       }
+      this.addAccount.add({ name, email, password })
     } catch (error) {
       return serverError()
     }
