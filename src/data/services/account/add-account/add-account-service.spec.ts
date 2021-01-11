13866d543a3661f979742c02f0e756b266d420de
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-11 00:44:34 -0300

    test: ensure AddAccountService calls Hasher with correct value

---
+++ src/data/services/account/add-account/add-account-service.spec.ts
@@ -0,0 +1,37 @@
+import { AddAccountService } from './add-account-service'
+import { Hasher } from '../../../contracts'
+
+const mockHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    async hash (value: string): Promise<string> {
+      return Promise.resolve('hash')
+    }
+  }
+  return new HasherStub()
+}
+
+interface SutTypes {
+  sut: AddAccountService,
+  hasherStub: Hasher
+}
+
+const makeSut = (): SutTypes => {
+  const hasherStub = mockHasher()
+  const sut = new AddAccountService(hasherStub)
+  return { sut, hasherStub }
+}
+
+describe('Add Account Service', () => {
+  test('Should call Hasher with correct value', async () => {
+    const { sut, hasherStub } = makeSut()
+    const hashSpy = jest.spyOn(hasherStub, 'hash')
+    const data =
+    {
+      name: 'any_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password'
+    }
+    await sut.add(data)
+    expect(hashSpy).toHaveBeenCalledWith(data.password)
+  })
+})
