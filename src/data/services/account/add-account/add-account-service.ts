a72087cde8c87e875762adc8f3f77a2521b7cedf
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-11 03:17:25 -0300

    feat: ensure AddAccountService calls method add of AccountRepository with correct values

--- src/data/services/account/add-account/add-account-service.ts
+++ src/data/services/account/add-account/add-account-service.ts
@@ -1,15 +1,17 @@
 import { AccountDTO } from '../../../../domain/data-transfer-objects'
 import { Account } from '../../../../domain/entities'
 import { AddAccount } from '../../../../domain/use-cases/add-account'
-import { Hasher } from '../../../contracts'
+import { AddAccountRepository, Hasher } from '../../../contracts'
 
 export class AddAccountService implements AddAccount {
   constructor (
-    private readonly hasher: Hasher
+    private readonly hasher: Hasher,
+    private readonly accountRepository: AddAccountRepository
   ) {}
 
   public async add (data: AccountDTO): Promise<Account> {
-    await this.hasher.hash(data.password)
+    const password = await this.hasher.hash(data.password)
+    await this.accountRepository.add({ ...data, password })
     return Promise.resolve(null)
   }
 }
