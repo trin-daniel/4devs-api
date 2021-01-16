63ca219bcd5f18aef6eff57e2edf1551ab77a981
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-16 01:43:32 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,7 @@
+import { RequestHandler } from 'express'
+export const cors: RequestHandler = (request, response, next) => {
+  response.set('access-control-allow-origin', '*')
+  response.set('access-control-allow-methods', '*')
+  response.set('access-control-allow-headers', '*')
+  next()
+}
