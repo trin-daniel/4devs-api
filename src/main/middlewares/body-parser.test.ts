b0f511b7289384a90266c737d51c8bd95a434c33
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-16 01:21:28 -0300

    test: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,23 @@
+import supertest from 'supertest'
+import app from '../config/app'
+
+describe('Body parser middleware', () => {
+  test('Should parse body as JSON', async () => {
+    const data =
+    {
+      name: 'any_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password',
+      confirmation: 'any_password'
+    }
+    app
+      .post('/test_body_parser', (request, response, next) => {
+        response.send(request.body)
+        next()
+      })
+    await supertest(app)
+      .post('/test_body_parser')
+      .send(data)
+      .expect(data)
+  })
+})
