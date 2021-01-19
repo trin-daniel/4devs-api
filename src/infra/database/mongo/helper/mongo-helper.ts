c56ed4ac23f6ec80882310a2139b8d16a1d1d102
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-19 02:55:11 -0300

    feat: ensure reconnect to the database if the connection is broken

--- src/infra/database/mongo/helper/mongo-helper.ts
+++ src/infra/database/mongo/helper/mongo-helper.ts
@@ -3,8 +3,10 @@ import { MongoClient } from 'mongodb'
 export const MongoHelper =
 {
   client: null as MongoClient,
+  uri: null as string,
   async connect (uri: string) {
-    this.client = await MongoClient.connect(uri,
+    this.uri = uri
+    this.client = await MongoClient.connect(this.uri,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -16,11 +18,14 @@ export const MongoHelper =
     this.client = null
   },
 
-  collection (name: string) {
+  async collection (name: string) {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   },
 
-  mapper <M = any> (collection: any): M {
+  mapper<M = any> (collection: any): M {
     const { _id, ...obj } = collection
     return Object.assign({}, obj, { id: _id })
   }
--- src/infra/database/mongo/repositories/account/account-repository.ts
+++ src/infra/database/mongo/repositories/account/account-repository.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../../helper/mongo-helper'
 
 export class AccountRepository implements AddAccountRepository {
   public async add (data: AccountDTO): Promise<Account> {
-    const collection = MongoHelper.collection('accounts')
+    const collection = await MongoHelper.collection('accounts')
     const { ops } = await collection.insertOne(data)
     const [res] = ops
     return MongoHelper.mapper<Account>(res)
