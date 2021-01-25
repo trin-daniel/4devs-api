ad0b0e4677e869e631546ce5e7936b82d1026371
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-25 03:32:09 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/signin/signin-controller.spec.ts
+++ src/presentation/controllers/signin/signin-controller.spec.ts
@@ -2,6 +2,7 @@ import { SigninController } from './signin-controller'
 import { EmailValidator } from '../../contracts/email-validator'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
+import { Request } from '../../contracts'
 
 interface SutTypes {
   sut: SigninController,
@@ -17,6 +18,14 @@ const mockEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
+const mockRequest = (): Request => ({
+  body:
+  {
+    email: 'any_email@gmail.com',
+    password: 'any_password'
+  }
+})
+
 const makeSut = (): SutTypes => {
   const emailValidatorStub = mockEmailValidator()
   const sut = new SigninController(emailValidatorStub)
@@ -26,26 +35,16 @@ const makeSut = (): SutTypes => {
 describe('Signin Controller', () => {
   test('Should return 400 if no email address is provided', async () => {
     const { sut } = makeSut()
-    const request =
-    {
-      body:
-      {
-        password: 'any_password'
-      }
-    }
+    const data = mockRequest()
+    const request = Object.assign({}, delete data.body.email, { body: { ...data.body } })
     const response = await sut.handle(request)
     expect(response).toEqual(badRequest(new MissingParamError('email')))
   })
 
   test('Should return 400 if no password is provided', async () => {
     const { sut } = makeSut()
-    const request =
-    {
-      body:
-      {
-        email: 'any_email@gmail.com'
-      }
-    }
+    const data = mockRequest()
+    const request = Object.assign({}, delete data.body.password, { body: { ...data.body } })
     const response = await sut.handle(request)
     expect(response).toEqual(badRequest(new MissingParamError('password')))
   })
@@ -53,14 +52,7 @@ describe('Signin Controller', () => {
   test('Should call EmailValidator with correct e-mail address', async () => {
     const { sut, emailValidatorStub } = makeSut()
     const isEmailSpy = jest.spyOn(emailValidatorStub, 'isEmail')
-    const request =
-    {
-      body:
-      {
-        email: 'any_email@gmail.com',
-        password: 'any_password'
-      }
-    }
+    const request = mockRequest()
     await sut.handle(request)
     expect(isEmailSpy).toHaveBeenCalledWith(request.body.email)
   })
@@ -68,14 +60,8 @@ describe('Signin Controller', () => {
   test('Should return 400 if an invalid e-mail address is provided', async () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isEmail').mockReturnValueOnce(false)
-    const request =
-    {
-      body:
-      {
-        email: 'any_email@gmail.com',
-        password: 'any_password'
-      }
-    }
+    const data = mockRequest()
+    const request = Object.assign({}, { body: { ...data.body, email: 'invalid_email@gmail.com' } })
     const response = await sut.handle(request)
     expect(response).toEqual(badRequest(new InvalidParamError('email')))
   })
