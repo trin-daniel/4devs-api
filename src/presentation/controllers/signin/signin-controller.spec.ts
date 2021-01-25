07f2d7da37b50ddd1fcb49c0fcc77c15cf3db3f1
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-25 02:38:54 -0300

    test: ensure SigninController calls EmailValidator with correct email

--- src/presentation/controllers/signin/signin-controller.spec.ts
+++ src/presentation/controllers/signin/signin-controller.spec.ts
@@ -1,14 +1,26 @@
+import { SigninController } from './signin-controller'
+import { EmailValidator } from '../../contracts/email-validator'
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
-import { SigninController } from './signin-controller'
 
 interface SutTypes {
-  sut: SigninController
+  sut: SigninController,
+  emailValidatorStub: EmailValidator
+}
+
+const mockEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    public isEmail (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new SigninController()
-  return { sut }
+  const emailValidatorStub = mockEmailValidator()
+  const sut = new SigninController(emailValidatorStub)
+  return { sut, emailValidatorStub }
 }
 
 describe('Signin Controller', () => {
@@ -18,7 +30,7 @@ describe('Signin Controller', () => {
     {
       body:
       {
-        password: '123456'
+        password: 'any_password'
       }
     }
     const response = await sut.handle(request)
@@ -36,5 +48,20 @@ describe('Signin Controller', () => {
     }
     const response = await sut.handle(request)
     expect(response).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isEmailSpy = jest.spyOn(emailValidatorStub, 'isEmail')
+    const request =
+    {
+      body:
+      {
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+    await sut.handle(request)
+    expect(isEmailSpy).toHaveBeenCalledWith(request.body.email)
   })
 })
