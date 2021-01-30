2dd1b56171e83abb424bebc0917daf33b0dc6768
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-30 07:50:33 -0300

    feat: ensure ValidatorComposite returns the first error if more then one validators fails

---
+++ src/validation/validators/validator-composite.ts
@@ -0,0 +1,15 @@
+import { Validator } from '../../presentation/contracts/validator'
+
+export class ValidatorComposite implements Validator {
+  constructor (
+    private readonly validators: Validator[]
+  ) {}
+
+  validate (input: { [key: string]: any; }): Error {
+    for (const validator of this.validators) {
+      const error = validator.validate(input)
+      if (error) return error
+    }
+    return null
+  }
+}
