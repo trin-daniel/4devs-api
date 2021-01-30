5d2d4b4d19f946f7974b6ea24d913e0bab33c59e
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-30 07:06:58 -0300

    feat: ensure CompareFieldsValidator returns an InvalidParamError if comparison fails

---
+++ src/validation/validators/compare-fields-validator.ts
@@ -0,0 +1,15 @@
+import { Validator } from '../../presentation/contracts/validator'
+import { InvalidParamError } from '../../presentation/errors'
+
+export class CompareFieldsValidator implements Validator {
+  constructor (
+    private readonly fieldName: string,
+    private readonly fieldToCompareName: string
+  ) {}
+
+  validate (input: {[key: string]: any}): Error | null {
+    return input[this.fieldName] !== input[this.fieldToCompareName]
+      ? new InvalidParamError(this.fieldToCompareName)
+      : null
+  }
+}
