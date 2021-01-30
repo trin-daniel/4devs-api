c563c6d58459350d11149f3e29f262f2d4ccd6b8
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-30 06:53:42 -0300

    test: ensure RequiredFieldValidator returns null if all fields are provided

--- src/validation/validators/required-field-validator.spec.ts
+++ src/validation/validators/required-field-validator.spec.ts
@@ -17,12 +17,19 @@ const data = () =>
     confirmation: 'any_password'
   })
 
-describe('Required Field', () => {
+describe('Required Field Validator', () => {
   test('Should return an MissingParamError if any field not provided', () => {
     const { sut } = makeSut()
     const objectBase = data()
     const input = Object.assign({}, objectBase, delete objectBase.name)
     const error = sut.validate(input)
     expect(error).toEqual(new MissingParamError('name'))
+  })
+
+  test('Should return null if all fields are provided', () => {
+    const { sut } = makeSut()
+    const input = data()
+    const error = sut.validate(input)
+    expect(error).toBeNull()
   })
 })
