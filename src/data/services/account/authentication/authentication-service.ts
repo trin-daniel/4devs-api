eb54644f741eb392a7603a23ef94b173723cb73c
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-31 04:36:42 -0300

    feat: ensure AuthenticationService calls HashCompare with correct values

--- src/data/services/account/authentication/authentication-service.ts
+++ src/data/services/account/authentication/authentication-service.ts
@@ -1,15 +1,19 @@
 import { Authentication } from '../../../../domain/use-cases/authentication/authentication'
 import { AuthenticationDTO } from '../../../../domain/data-transfer-objects'
-import { LoadAccountByEmailRepository } from '../../../contracts'
+import { HashCompare, LoadAccountByEmailRepository } from '../../../contracts'
 
 export class AuthenticationService implements Authentication {
   constructor (
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashCompare: HashCompare
   ) {}
 
   async auth (data: AuthenticationDTO): Promise<string> {
-    const { email } = data
-    await this.loadAccountByEmailRepository.loadByEmail(email)
+    const { email, password } = data
+    const account = await this.loadAccountByEmailRepository.loadByEmail(email)
+    if (account) {
+      await this.hashCompare.compare(password, account.password)
+    }
     return null
   }
 }
