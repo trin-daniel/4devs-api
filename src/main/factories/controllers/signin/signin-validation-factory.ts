c4005c0dfe8ae0e04ef8ec82efe9b45b5f1e5327
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-31 02:00:21 -0300

    feat: ensure SigninValidationFactory calls ValidatorComposite with all validators

---
+++ src/main/factories/controllers/signin/signin-validation-factory.ts
@@ -0,0 +1,13 @@
+import { Validator } from '../../../../presentation/contracts/validator'
+import { EmailValidatorAdapter } from '../../../../utils/email-validator-adapter'
+import { EmailFieldValidator, RequiredFieldValidator, ValidatorComposite } from '../../../../validation/validators'
+
+export const signinValidationFactory = (): Validator => {
+  const validators: Validator[] = []
+  const requiredFields = ['email', 'password']
+  for (const field of requiredFields) {
+    validators.push(new RequiredFieldValidator(field))
+  }
+  validators.push(new EmailFieldValidator('email', new EmailValidatorAdapter()))
+  return new ValidatorComposite(validators)
+}
