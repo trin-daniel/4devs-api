a1f7c723a2cc7b9a389ef765b23190e5c7b614fe
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-31 05:07:05 -0300

    feat: ensure AuthenticationService calls Encrypter with correct id

--- src/data/services/account/authentication/authentication-service.ts
+++ src/data/services/account/authentication/authentication-service.ts
@@ -1,18 +1,22 @@
 import { Authentication } from '../../../../domain/use-cases/authentication/authentication'
 import { AuthenticationDTO } from '../../../../domain/data-transfer-objects'
-import { HashCompare, LoadAccountByEmailRepository } from '../../../contracts'
+import { Encrypter, HashCompare, LoadAccountByEmailRepository } from '../../../contracts'
 
 export class AuthenticationService implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashCompare: HashCompare
+    private readonly hashCompare: HashCompare,
+    private readonly encrypter: Encrypter
   ) {}
 
   async auth (data: AuthenticationDTO): Promise<string> {
     const { email, password } = data
     const account = await this.loadAccountByEmailRepository.loadByEmail(email)
     if (account) {
-      await this.hashCompare.compare(password, account.password)
+      const isEqual = await this.hashCompare.compare(password, account.password)
+      return isEqual
+        ? this.encrypter.encrypt(account.id)
+        : null
     }
     return null
   }
