26ac6caefab716e41a9078133df2618576476510
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-30 22:44:54 -0300

    test: ensure SignupValidationFactory calls ValidatorComposite with all validators

---
+++ src/main/factories/controllers/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,29 @@
+import { signupValidationFactory } from './signup-validation-factory'
+import { Validator } from '../../../../presentation/contracts/validator'
+import { EmailValidator } from '../../../../presentation/contracts/email-validator'
+import { CompareFieldsValidator, EmailFieldValidator, RequiredFieldValidator, ValidatorComposite } from '../../../../validation/validators'
+
+const mockEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isEmail (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+jest.mock('../../../../validation/validators/validator-composite')
+
+describe('Signup Validation Factory', () => {
+  test('Should call ValidatorComposite with all validators', () => {
+    signupValidationFactory()
+    const validators: Validator[] = []
+    const requiredFields = ['name', 'email', 'password', 'confirmation']
+    for (const field of requiredFields) {
+      validators.push(new RequiredFieldValidator(field))
+    }
+    validators.push(new EmailFieldValidator('email', mockEmailValidator()))
+    validators.push(new CompareFieldsValidator('password', 'confirmation'))
+    expect(ValidatorComposite).toHaveBeenCalledWith(validators)
+  })
+})
