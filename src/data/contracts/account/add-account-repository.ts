2bbc2db5b4f7536af3afb58b8ebe8d27cd2fbef1
Carlos Daniel <trindadedanniel63@outlook.com>
2021-01-31 05:51:26 -0300

    refactor: move interfaces to their own folder

--- src/data/contracts/account/add-account-repository.ts
+++
@@ -1,6 +0,0 @@
-import { Account } from '../../../domain/entities'
-import { AccountDTO } from '../../../domain/data-transfer-objects'
-
-export interface AddAccountRepository {
-  add (data: AccountDTO): Promise<Account>
-}
