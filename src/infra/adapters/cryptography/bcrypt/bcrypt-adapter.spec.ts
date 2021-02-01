7bed43e6d351f9c02464b4228626a1d027c1fd0a
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-01 03:11:43 -0300

    test: ensure BcryptAdapter throws if compare method throws

--- src/infra/adapters/cryptography/bcrypt/bcrypt-adapter.spec.ts
+++ src/infra/adapters/cryptography/bcrypt/bcrypt-adapter.spec.ts
@@ -64,5 +64,12 @@ describe('Bcrypt Adapter', () => {
       const isValid = await sut.compare('any_password', 'any_hash')
       expect(isValid).toBe(false)
     })
+
+    test('Should throw if compare method throws', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(Promise.reject(new Error()))
+      const promise = sut.compare('any_password', 'any_hash')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
