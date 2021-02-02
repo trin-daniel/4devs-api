b4bced42f63de1d69884569fc5415688e57525c0
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-02 00:26:38 -0300

    test: ensure AccountRepository returns an account if load by e-mail address succeeds

--- src/infra/database/mongo/repositories/account/account-repository.spec.ts
+++ src/infra/database/mongo/repositories/account/account-repository.spec.ts
@@ -19,6 +19,7 @@ describe('Account Repository', () => {
     const collection = await MongoHelper.collection('accounts')
     await collection.deleteMany({})
   })
+
   describe('#AddAccountRepository', () => {
     test('Should return an account on success', async () => {
       const { sut } = makeSut()
@@ -33,6 +34,24 @@ describe('Account Repository', () => {
       expect(account.id).toBeTruthy()
       expect(account.name).toBe('any_name')
       expect(account.email).toBe('any_email@gmail.com')
+    })
+  })
+
+  describe('#LoadAccountByEmailRepository', () => {
+    test('Should return an account if load by e-mail address succeeds', async () => {
+      const { sut } = makeSut()
+      const data =
+      {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'hash'
+      }
+      const collection = await MongoHelper.collection('accounts')
+      await collection.insertOne(data)
+      const account = await sut.loadByEmail('any_email@gmail.com')
+      expect(account).toBeTruthy()
+      expect(account.id).toHaveProperty('id')
+      expect(account).toHaveProperty('name')
     })
   })
 })
