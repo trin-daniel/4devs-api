af88420ec046ad3d8dd392f194779a9527b78073
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-02 00:26:15 -0300

    feat: ensure AccountRepository returns an account if load by e-mail address succeeds

--- src/infra/database/mongo/repositories/account/account-repository.ts
+++ src/infra/database/mongo/repositories/account/account-repository.ts
@@ -1,13 +1,19 @@
-import { AddAccountRepository } from '../../../../../data/contracts'
+import { AddAccountRepository, LoadAccountByEmailRepository } from '../../../../../data/contracts'
 import { Account } from '../../../../../domain/entities'
 import { AccountDTO } from '../../../../../domain/data-transfer-objects'
 import { MongoHelper } from '../../helper/mongo-helper'
 
-export class AccountRepository implements AddAccountRepository {
+export class AccountRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   public async add (data: AccountDTO): Promise<Account> {
     const collection = await MongoHelper.collection('accounts')
     const { ops } = await collection.insertOne(data)
     const [res] = ops
     return MongoHelper.mapper<Account>(res)
+  }
+
+  async loadByEmail (email: string): Promise<Account> {
+    const collection = await MongoHelper.collection('accounts')
+    const account = await collection.findOne({ email })
+    return MongoHelper.mapper<Account>(account)
   }
 }
