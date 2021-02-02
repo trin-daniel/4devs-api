2792d254cce25ed9f01905603096e34772f31169
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-02 04:05:07 -0300

    test: ensure SigninRoute returns 200 if authentication succeeds

---
+++ src/main/routes/signin-route.test.ts
@@ -0,0 +1,37 @@
+import { MongoHelper } from '../../infra/database/mongo/helper/mongo-helper'
+import app from '../config/app'
+import supertest from 'supertest'
+import bcrypt from 'bcrypt'
+
+describe('Signin Route', () => {
+  beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
+  afterAll(async () => await MongoHelper.disconnect())
+  beforeEach(async () => {
+    const collection = await MongoHelper.collection('accounts')
+    await collection.deleteMany({})
+  })
+
+  describe('#POST/Signin', () => {
+    test('Should return 200 if authentication succeeds', async () => {
+      const collection = await MongoHelper.collection('accounts')
+      const data =
+      {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: await bcrypt.hash('any_password', 12)
+      }
+
+      const credentials =
+      {
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+
+      await collection.insertOne(data)
+      await supertest(app)
+        .post('/api/signin')
+        .send(credentials)
+        .expect(200)
+    })
+  })
+})
