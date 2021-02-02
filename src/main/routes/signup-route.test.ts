90a3acfce8a94b013d68a170ee91cbc57883560d
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-02 04:08:51 -0300

    refactor: move all tests to the separate describe

--- src/main/routes/signup-route.test.ts
+++ src/main/routes/signup-route.test.ts
@@ -1,26 +1,30 @@
-import supertest from 'supertest'
 import { MongoHelper } from '../../infra/database/mongo/helper/mongo-helper'
 import app from '../config/app'
+import supertest from 'supertest'
 
 describe('Signup Route', () => {
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
   afterAll(async () => await MongoHelper.disconnect())
+
   beforeEach(async () => {
     const collection = await MongoHelper.collection('accounts')
     await collection.deleteMany({})
   })
 
-  test('Should return 200 on sucess', async () => {
-    const data =
-    {
-      name: 'any_name',
-      email: 'any_email@gmail.com',
-      password: 'any_password',
-      confirmation: 'any_password'
-    }
-    await supertest(app)
-      .post('/api/signup')
-      .send(data)
-      .expect(200)
+  describe('#POST/Signup', () => {
+    test('Should return 200 when adding account successfully', async () => {
+      const data =
+      {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        confirmation: 'any_password'
+      }
+
+      await supertest(app)
+        .post('/api/signup')
+        .send(data)
+        .expect(200)
+    })
   })
 })
