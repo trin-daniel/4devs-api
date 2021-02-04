99d268f934547b7c4e455cd803c62bee28e3950a
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-03 23:47:36 -0300

    chore: add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:14
 WORKDIR /usr/src/4devs-api
 COPY ./package.json .
 RUN npm install --only=prod
-COPY ./dist ./dist
-EXPOSE 3333
-CMD npm start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  HOST: process.env.MONGO_URL || 'mongodb://localhost:27017/4devs-api',
+  HOST: process.env.MONGO_URL || 'mongodb://mongo:27017/4devs-api',
   PORT: process.env.PORT || 3333,
   SECRET_KEY: process.env.JWT_SECRET || 'TJ001ERFG'
 }
