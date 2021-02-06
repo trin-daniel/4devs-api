8c214d1d74bc401dca08e01b4ebe0c020ac410c4
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-06 04:13:11 -0300

    feat: ensure AddAccountService calls LoadAccountByEmailRepository with correct e-mail address

--- src/data/services/account/add-account/add-account-service.ts
+++ src/data/services/account/add-account/add-account-service.ts
@@ -1,15 +1,17 @@
 import { AccountDTO } from '../../../../domain/data-transfer-objects'
 import { Account } from '../../../../domain/entities'
 import { AddAccount } from '../../../../domain/use-cases/account/add-account'
-import { AddAccountRepository, Hasher } from '../../../contracts'
+import { AddAccountRepository, Hasher, LoadAccountByEmailRepository } from '../../../contracts'
 
 export class AddAccountService implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly accountRepository: AddAccountRepository
+    private readonly accountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
   public async add (data: AccountDTO): Promise<Account> {
+    await this.loadAccountByEmailRepository.loadByEmail(data.email)
     const password = await this.hasher.hash(data.password)
     const account = await this.accountRepository.add({ ...data, password })
     return account
