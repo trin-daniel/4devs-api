e776351373fc6f16d3b51a808c8e767da54c7ee1
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-06 01:30:26 -0300

    feat: add AuthenticationService on SignupControllerFactory

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -6,13 +6,18 @@ import { AccountRepository } from '../../../../infra/database/mongo/repositories
 import { LogControllerDecorator } from '../../../decorators/controllers/log-controller-decorator'
 import { LogMongoRepository } from '../../../../infra/database/mongo/repositories/log-error/log-mongo-repository'
 import { signupValidationFactory } from './signup-validation-factory'
+import { AuthenticationService } from '../../../../data/services/account/authentication/authentication-service'
+import { JsonWebTokenAdapter } from '../../../../infra/adapters/cryptography/json-web-token/json-web-token-adapter'
+import env from '../../../config/env'
 
 export const signupControllerFactory = (): Controller => {
   const accountRepository = new AccountRepository()
   const hasher = new BcryptAdapter(12)
+  const jsonWebTokenAdapter = new JsonWebTokenAdapter(env.SECRET_KEY)
+  const authentication = new AuthenticationService(accountRepository, hasher, jsonWebTokenAdapter, accountRepository)
   const addAccount = new AddAccountService(hasher, accountRepository)
   const signupValidation = signupValidationFactory()
-  const signupController = new SignupController(signupValidation, addAccount)
+  const signupController = new SignupController(signupValidation, addAccount, authentication)
   const LogErrorRepository = new LogMongoRepository()
   return new LogControllerDecorator(signupController, LogErrorRepository)
 }
