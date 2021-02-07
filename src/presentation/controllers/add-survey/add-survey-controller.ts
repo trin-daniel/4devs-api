18c92057e8d9b87dae85eaa214ae03e58ef37548
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-07 01:49:55 -0300

    feat: ensure AddSurveyController returns 400 if validator returns an error

--- src/presentation/controllers/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/add-survey/add-survey-controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Request, Response, Validator } from '../../contracts'
+import { badRequest } from '../../helpers/http-helper'
 
 export class AddSurveyController implements Controller {
   constructor (
@@ -7,7 +8,10 @@ export class AddSurveyController implements Controller {
 
   async handle (request: Request<any>): Promise<Response<any>> {
     const { question, answers } = request.body
-    this.validator.validate({ question, answers })
+    const error = this.validator.validate({ question, answers })
+    if (error) {
+      return badRequest(error)
+    }
     return Promise.resolve(null)
   }
 }
