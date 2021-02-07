cc2c996b7caf4a81b2318f39f2f7bedd7f4a55e7
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-07 10:26:11 -0300

    test: ensure SurveyMongoRepository add a new survey when it is successful

---
+++ src/infra/database/mongo/repositories/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,48 @@
+import { SurveyMongoRepository } from './survey-mongo-repository'
+import { SurveyDTO } from '../../../../../domain/data-transfer-objects'
+import { MongoHelper } from '../../helper/mongo-helper'
+
+const mockSurveyDTO = (): SurveyDTO => ({
+  question: 'any_question',
+  answers:
+    [
+      {
+        image: 'any_images',
+        answer: 'any_answer'
+      },
+      {
+        answer: 'another_answer'
+      }
+    ]
+})
+
+interface SutTypes {
+  sut: SurveyMongoRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new SurveyMongoRepository()
+  return { sut }
+}
+
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
+  afterAll(async () => await MongoHelper.disconnect())
+
+  beforeEach(async () => {
+    const collection = await MongoHelper.collection('surveys')
+    await collection.deleteMany({})
+  })
+
+  describe('#AddSurveyRepository', () => {
+    test('Should add a new survey when it is successful', async () => {
+      const { sut } = makeSut()
+      await sut.add(mockSurveyDTO())
+      const collection = await MongoHelper.collection('surveys')
+      const survey = await collection.findOne({ question: mockSurveyDTO().question })
+      expect(survey).toBeTruthy()
+      expect(survey).toHaveProperty('_id')
+      expect(survey).toHaveProperty('answers')
+    })
+  })
+})
