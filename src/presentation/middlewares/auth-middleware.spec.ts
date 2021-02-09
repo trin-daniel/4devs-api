735b22a874fc3e02af4c84ee438cf79912b08a54
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-09 08:14:34 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with the correct token property

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,10 +1,13 @@
 import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken } from '../../domain/use-cases/account/load-account-by-token'
 import { Request } from '../contracts'
 import { forbidden } from '../helpers/http-helper'
 import { AccessDeniedError } from '../errors'
+import { Account } from '../../domain/entities'
 
 interface SutTypes {
   sut: AuthMiddleware,
+  loadAccountByTokenStub: LoadAccountByToken
 }
 
 const mockRequest = (): Request => (
@@ -16,11 +19,39 @@ const mockRequest = (): Request => (
   }
 )
 
-const makeSut = (): SutTypes => {
-  const sut = new AuthMiddleware()
-  return { sut }
+const mockAccount = (): Account => (
+  {
+    id: '507f1f77bcf86cd799439011',
+    name: 'any_name',
+    email: 'any_email@gmail.com',
+    password: 'hash'
+  }
+)
+
+const mockLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (token: string): Promise<Account> {
+      return Promise.resolve(mockAccount())
+    }
+  }
+  return new LoadAccountByTokenStub()
 }
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenStub = mockLoadAccountByToken()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+  return { sut, loadAccountByTokenStub }
+}
+
 describe('Auth Middleware', () => {
+  test('Should call LoadAccountByToken with the correct token property', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(loadSpy).toHaveBeenCalledWith(request.headers['x-access-token'])
+  })
+
   test('Should return 403 if no x-access-token property exists in headers', async () => {
     const { sut } = makeSut()
     const request = mockRequest()
