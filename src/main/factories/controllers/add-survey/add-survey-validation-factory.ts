cd41e1d71af4f385a851e608927d9fb21f02359a
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-09 03:49:17 -0300

    feat: ensure AddSurveyValidationFactory calls ValidatorComposite with all validators

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
@@ -0,0 +1,9 @@
+import { Validator } from '../../../../presentation/contracts'
+import { RequiredFieldValidator, ValidatorComposite } from '../../../../validation/validators'
+
+export const AddSurveyValidationFactory = (): Validator => {
+  const validators: Validator[] = []
+  validators.push(new RequiredFieldValidator('question'))
+  validators.push(new RequiredFieldValidator('answers'))
+  return new ValidatorComposite(validators)
+}
