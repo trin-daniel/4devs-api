de84379010c0f87cbb38fc307bbeb08e38859db4
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-10 21:45:49 -0300

    test: ensure AddSurveyRoute returns 204 if valid token is provided

--- src/main/routes/add-survey-route.test.ts
+++ src/main/routes/add-survey-route.test.ts
@@ -1,6 +1,9 @@
 import { MongoHelper } from '../../infra/database/mongo/helper/mongo-helper'
 import app from '../config/app'
 import supertest from 'supertest'
+import env from '../config/env'
+import bcrypt from 'bcrypt'
+import jsonWebToken from 'jsonwebtoken'
 
 describe('Add Survey Route', () => {
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
@@ -26,6 +29,34 @@ describe('Add Survey Route', () => {
         .post('/api/surveys')
         .send(data)
         .expect(403)
+    })
+
+    test('Should return 204 if valid token is provided', async () => {
+      const credentials =
+      {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: await bcrypt.hash('any_password', 12),
+        role: 'admin'
+      }
+      const collection = await MongoHelper.collection('accounts')
+      const { ops } = await collection.insertOne(credentials)
+      const [res] = ops
+      const token = jsonWebToken.sign('any_token', env.SECRET_KEY)
+      await collection.updateOne(
+        { _id: res._id },
+        { $set: { token } }
+      )
+      const data =
+      {
+        question: 'any_question',
+        answers: [{ answer: 'any_answer', image: 'any_image' }]
+      }
+      await supertest(app)
+        .post('/api/surveys')
+        .set('x-access-token', token)
+        .send(data)
+        .expect(204)
     })
   })
 })
