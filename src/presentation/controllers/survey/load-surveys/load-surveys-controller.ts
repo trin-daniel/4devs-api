5aa151b46965067589a81e110a3a490ff930eacd
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-11 05:18:10 -0300

    feat: ensure LoadSurveysController returns 500 if LoadSurveys throws exception

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,6 +1,6 @@
 import { LoadSurveys } from '../../../../domain/use-cases/survey/load-surveys'
 import { Controller, Request, Response } from '../../../contracts'
-import { ok } from '../../../helpers/http-helper'
+import { ok, serverError } from '../../../helpers/http-helper'
 
 export class LoadSurveysController implements Controller {
   constructor (
@@ -8,7 +8,11 @@ export class LoadSurveysController implements Controller {
   ) {}
 
   async handle (request: Request): Promise<Response> {
-    const surveys = await this.loadSurveys.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurveys.load()
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
