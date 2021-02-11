be9b97db7884e20f8325414228039863c06fd721
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-11 08:18:52 -0300

    test: ensure LoadSurveysService throws if LoadSurveysRepository throws

--- src/data/services/survey/load-surveys/load-surveys-service.spec.ts
+++ src/data/services/survey/load-surveys/load-surveys-service.spec.ts
@@ -58,5 +58,12 @@ describe('Load Surveys Service', () => {
       const surveys = await sut.load()
       expect(surveys).toEqual(mockSurveys())
     })
+
+    test('Should throw if LoadSurveysRepository throws', async () => {
+      const { sut, loadSurveysRepositoryStub } = makeSut()
+      jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockReturnValueOnce(Promise.reject(new Error()))
+      const promise = sut.load()
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
