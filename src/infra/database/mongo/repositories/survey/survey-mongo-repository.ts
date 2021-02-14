0a76490d3301e964c53c9d4e5ac4bff5f81f3ec0
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-13 22:24:18 -0300

    feat: ensure SurveyMongoRepository returns a survey if load by id is successful

--- src/infra/database/mongo/repositories/survey/survey-mongo-repository.ts
+++ src/infra/database/mongo/repositories/survey/survey-mongo-repository.ts
@@ -4,10 +4,14 @@ import { MongoHelper } from '@infra/database/mongo/helper/mongo-helper'
 import
 {
   AddSurveyRepository,
+  LoadSurveyByIdRepository,
   LoadSurveysRepository
 } from '@data/contracts'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+export class SurveyMongoRepository implements
+AddSurveyRepository,
+LoadSurveysRepository,
+LoadSurveyByIdRepository {
   async add (data: SurveyDTO): Promise<void> {
     const collection = await MongoHelper.collection('surveys')
     await collection.insertOne(data)
@@ -17,5 +21,11 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
     const collection = await MongoHelper.collection('surveys')
     const surveys = await collection.find().toArray() as Surveys[]
     return surveys
+  }
+
+  async loadById (id: string): Promise<Surveys> {
+    const collection = await MongoHelper.collection('surveys')
+    const survey = await collection.findOne({ _id: id }) as Surveys
+    return survey
   }
 }
