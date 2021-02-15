87bbd19e040f2488fe47c1cbab419ffc25212309
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-14 21:44:41 -0300

    test: ensure SaveSurveyResultController returns 500 if LoadSurveyById throws exception

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -3,7 +3,7 @@ import { LoadSurveyById } from '@domain/use-cases/survey/load-survey-by-id'
 import { Request } from '@presentation/contracts'
 import { SaveSurveyResultController } from '@presentation/controllers/survey-result/save-survey-result/save-survey-result-controller'
 import { InvalidParamError } from '@presentation/errors'
-import { forbidden } from '@presentation/helpers/http-helper'
+import { forbidden, serverError } from '@presentation/helpers/http-helper'
 
 type SutTypes = {
   sut: SaveSurveyResultController
@@ -60,6 +60,14 @@ describe('Save Survey Result Controller', () => {
       jest.spyOn(loadSurveyByIdStub, 'load').mockResolvedValue(null)
       const response = await sut.handle(request)
       expect(response).toEqual(forbidden(new InvalidParamError('survey_id')))
+    })
+
+    test('Should return 500 if LoadSurveyById throws exception', async () => {
+      const { sut, loadSurveyByIdStub } = makeSut()
+      const request = mockRequest()
+      jest.spyOn(loadSurveyByIdStub, 'load').mockRejectedValueOnce(new Error())
+      const response = await sut.handle(request)
+      expect(response).toEqual(serverError(new Error()))
     })
   })
 })
