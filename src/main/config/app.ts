2dc076c1087b0fd40cc29064b76dcab6c3194450
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-17 23:51:57 -0300

    feat: add documentation index file

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import setupMiddlewares from '@main/config/middlewares'
 import setupRoutes from '@main/config/routes'
 import express from 'express'
+import setupSwagger from '@main/config/swagger'
 
 const app = express()
+setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,9 @@
+export default {
+  openapi: '3.0.0',
+  info:
+  {
+    title: '4Dev-API',
+    description: 'API produzida durante o treinamento do instrutor Rodrigo Manguinho',
+    version: '1.0.0'
+  }
+}
