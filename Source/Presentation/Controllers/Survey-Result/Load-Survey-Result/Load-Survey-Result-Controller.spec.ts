c9b3c061ae6aca53a92fbe95b1230b34b7f878a0
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-23 08:09:59 -0300

    test: ensure LoadSurveyResultController calls LoadSurveyResultUseCase with correct value

--- Source/Presentation/Controllers/Survey-Result/Load-Survey-Result/Load-Survey-Result-Controller.spec.ts
+++ Source/Presentation/Controllers/Survey-Result/Load-Survey-Result/Load-Survey-Result-Controller.spec.ts
@@ -1,9 +1,10 @@
-import { Surveys } from '@Application/Entities'
+import { SurveyResult, Surveys } from '@Application/Entities'
 import { LoadSurveyByIdUseCase } from '@Application/Use-Cases/Survey/Load-Survey-By-Id-Use-Case'
 import { Request } from '@Presentation/Protocols'
 import { LoadSurveyResultController } from '@Presentation/Controllers/Survey-Result/Load-Survey-Result/Load-Survey-Result-Controller'
 import { Forbidden, ServerErrorHelper } from '@Presentation/Helpers/Http-Helper'
 import { InvalidParamError } from '@Presentation/Errors'
+import { LoadSurveyResultUseCase } from '@Application/Use-Cases/Survey-Result/Load-Survey-Result-Use-Case'
 
 const MockRequest = (): Request =>
   (
@@ -25,6 +26,23 @@ const MockSurveys = (): Surveys =>
     }
   )
 
+const MockSurveyResult = (): SurveyResult => (
+  {
+    id: '507f1f77bcf86cd799439011',
+    question: 'any_question',
+    survey_id: 'any_survey_id',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer',
+        count: 1,
+        percent: 50
+      }
+    ],
+    date: new Date().toLocaleDateString('pt-br')
+  }
+)
+
 const MockLoadSurveyByIdUseCase = (): LoadSurveyByIdUseCase => {
   class LoadSurveyByIdUseCaseStub implements LoadSurveyByIdUseCase {
     async Load (id: string): Promise<Surveys> {
@@ -34,12 +52,22 @@ const MockLoadSurveyByIdUseCase = (): LoadSurveyByIdUseCase => {
   return new LoadSurveyByIdUseCaseStub()
 }
 
-type SutTypes ={Sut: LoadSurveyResultController, LoadSurveyByIdUseCaseStub: LoadSurveyByIdUseCase}
+const MockLoadSurveyResultUseCase = (): LoadSurveyResultUseCase => {
+  class LoadSurveyResultUseCaseStub implements LoadSurveyResultUseCase {
+    Load (survey_id: string): Promise<SurveyResult> {
+      return Promise.resolve(MockSurveyResult())
+    }
+  }
+  return new LoadSurveyResultUseCaseStub()
+}
+
+type SutTypes ={Sut: LoadSurveyResultController, LoadSurveyByIdUseCaseStub: LoadSurveyByIdUseCase, LoadSurveyResultUseCaseStub: LoadSurveyResultUseCase}
 
 const makeSut = (): SutTypes => {
+  const LoadSurveyResultUseCaseStub = MockLoadSurveyResultUseCase()
   const LoadSurveyByIdUseCaseStub = MockLoadSurveyByIdUseCase()
-  const Sut = new LoadSurveyResultController(LoadSurveyByIdUseCaseStub)
-  return { Sut, LoadSurveyByIdUseCaseStub }
+  const Sut = new LoadSurveyResultController(LoadSurveyByIdUseCaseStub, LoadSurveyResultUseCaseStub)
+  return { Sut, LoadSurveyByIdUseCaseStub, LoadSurveyResultUseCaseStub }
 }
 
 describe('Load Survey Result Controller', () => {
@@ -65,6 +93,17 @@ describe('Load Survey Result Controller', () => {
       jest.spyOn(LoadSurveyByIdUseCaseStub, 'Load').mockRejectedValueOnce(new Error())
       const Response = await Sut.handle(MockRequest())
       expect(Response).toEqual(ServerErrorHelper(new Error()))
+    })
+  })
+
+  describe('#LoadSurveyResultUseCase', () => {
+    test('Should call LoadSurveyResultUseCase with correct value', async () => {
+      const { Sut, LoadSurveyResultUseCaseStub } = makeSut()
+      const LoadSpy = jest.spyOn(LoadSurveyResultUseCaseStub, 'Load')
+      const Request = MockRequest()
+      const { params: { survey_id } } = Request
+      await Sut.handle(MockRequest())
+      expect(LoadSpy).toHaveBeenCalledWith(survey_id)
     })
   })
 })
