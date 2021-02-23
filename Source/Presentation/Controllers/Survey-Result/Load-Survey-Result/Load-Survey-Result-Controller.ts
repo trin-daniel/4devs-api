d520a1c7b7842efff5460b24e4b576738bc11f55
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-23 08:17:08 -0300

    feat: ensure LoadSurveyResultController returns 200 if all use cases are successful

--- Source/Presentation/Controllers/Survey-Result/Load-Survey-Result/Load-Survey-Result-Controller.ts
+++ Source/Presentation/Controllers/Survey-Result/Load-Survey-Result/Load-Survey-Result-Controller.ts
@@ -1,7 +1,7 @@
 import { LoadSurveyResultUseCase } from '@Application/Use-Cases/Survey-Result/Load-Survey-Result-Use-Case'
 import { LoadSurveyByIdUseCase } from '@Application/Use-Cases/Survey/Load-Survey-By-Id-Use-Case'
 import { InvalidParamError } from '@Presentation/Errors'
-import { Forbidden, ServerErrorHelper } from '@Presentation/Helpers/Http-Helper'
+import { Forbidden, Ok, ServerErrorHelper } from '@Presentation/Helpers/Http-Helper'
 import { Controller, Request, Response } from '@Presentation/Protocols'
 
 export class LoadSurveyResultController implements Controller {
@@ -15,8 +15,8 @@ export class LoadSurveyResultController implements Controller {
       const { params: { survey_id } } = request
       const Survey = await this.LoadSurveyById.Load(survey_id)
       if (!Survey) return Forbidden(new InvalidParamError('survey_id'))
-      await this.LoadSurveyResult.Load(survey_id)
-      return null
+      const SurveyResult = await this.LoadSurveyResult.Load(survey_id)
+      return Ok(SurveyResult)
     } catch (error) {
       return ServerErrorHelper(error)
     }
