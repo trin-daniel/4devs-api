a7c755f581d3326a5c70c3f95663134a7ebfe4a8
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-23 07:57:13 -0300

    feat: ensure LoadSurveyResultController return 500 if LoadSurveyByIdUseCase throws exception

--- Source/Presentation/Controllers/Survey-Result/Load-Survey-Result/Load-Survey-Result-Controller.ts
+++ Source/Presentation/Controllers/Survey-Result/Load-Survey-Result/Load-Survey-Result-Controller.ts
@@ -1,6 +1,6 @@
 import { LoadSurveyByIdUseCase } from '@Application/Use-Cases/Survey/Load-Survey-By-Id-Use-Case'
 import { InvalidParamError } from '@Presentation/Errors'
-import { Forbidden } from '@Presentation/Helpers/Http-Helper'
+import { Forbidden, ServerErrorHelper } from '@Presentation/Helpers/Http-Helper'
 import { Controller, Request, Response } from '@Presentation/Protocols'
 
 export class LoadSurveyResultController implements Controller {
@@ -9,9 +9,13 @@ export class LoadSurveyResultController implements Controller {
   ) {}
 
   async handle (request: Request): Promise<Response> {
-    const { params: { survey_id } } = request
-    const Survey = await this.LoadSurveyById.Load(survey_id)
-    if (!Survey) return Forbidden(new InvalidParamError('survey_id'))
-    return null
+    try {
+      const { params: { survey_id } } = request
+      const Survey = await this.LoadSurveyById.Load(survey_id)
+      if (!Survey) return Forbidden(new InvalidParamError('survey_id'))
+      return null
+    } catch (error) {
+      return ServerErrorHelper(error)
+    }
   }
 }
