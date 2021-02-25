80aa81a84ccc0de0e2a8a5e97f392f37ba26f73b
Carlos Daniel <trindadedanniel63@outlook.com>
2021-02-25 05:15:19 -0300

    fix: ensure ExpressMiddlewareAdapter use headers http

--- Source/Main/Adapters/Express/Express-Middleware-Adapter.ts
+++ Source/Main/Adapters/Express/Express-Middleware-Adapter.ts
@@ -1,15 +1,16 @@
-import { Middleware, Request, Response } from '@Presentation/Protocols'
+import { Middleware, Response } from '@Presentation/Protocols'
 import { RequestHandler } from 'express'
 
 export const ExpressMiddlewareAdapter = (middleware: Middleware): RequestHandler => {
   return async (req, res, next) => {
-    const Req: Request = {
+    const Req = {
+      token: req.headers?.['x-access-token'],
       headers: req.headers
     }
 
     const Res: Response = await middleware.handle(Req)
     if (Res.statusCode === 200) {
-      Object.assign(req, Req.body)
+      Object.assign(req, Res.body)
       next()
     } else {
       res.status(403).json({ error: Res.body.message })
